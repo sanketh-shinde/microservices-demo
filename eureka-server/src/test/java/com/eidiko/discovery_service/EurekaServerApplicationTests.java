9d7ed0f412e1a9422850e7581d8891384499be99
Sanketh Shinde <sankethshinde99@gmail.com>
2024-07-23 10:29:57 +0530

    add eureka server

---
+++ eureka-server/src/test/java/com/eidiko/discovery_service/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eidiko.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
